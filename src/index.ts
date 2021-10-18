ba8405be033e68c2304146e4e2611728d891d89f
Joseph Harrison-Lim <joseph@pixleeteam.com>
2021-10-18 01:08:05 -0400

    asdf

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import http from 'http';
 import { Server } from 'socket.io';
 import { AddressInfo } from 'net';
 import cors from 'cors';
+import { join } from 'path';
 
 interface Room {
   host: string;
@@ -37,8 +38,8 @@ export class Application {
     this._express = express();
     this._express.use(cors());
     const staticPath = process.env.NODE_ENV === 'production'
-      ? 'build'
-      : 'frontend/build';
+      ? join(__dirname, 'dist', 'build')
+      : join(__dirname, 'build');
     console.log('static path: ', staticPath);
     this._express.use(express.static(staticPath));
     this._srv = http.createServer(this._express);
